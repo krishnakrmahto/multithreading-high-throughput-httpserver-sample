d32ea677a12c1b0a7c04d5afc5f5b2f97851bcab
krishnakrmahto97 <krish.thorcode@gmail.com>
2022-03-13 19:30:06 +0530

    added httphandler - wordcounthandler

---
+++ src/performance/throughput/httprequesthandler/WordCountHandler.java
@@ -0,0 +1,58 @@
+package performance.throughput.httprequesthandler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * The implementation does not consider delimitation, any occurrence of the sequence of characters
+ * will be considered as a valid occurrence.
+ */
+public class WordCountHandler implements HttpHandler {
+
+  private final String textToSearchIn;
+  public WordCountHandler(String textToSearchIn) {
+    this.textToSearchIn = textToSearchIn;
+  }
+
+  @Override
+  public void handle(HttpExchange httpExchange) throws IOException {
+    String query = httpExchange.getRequestURI().getQuery();
+    String[] keyValue = query.split("=");
+
+    String key = keyValue[0];
+    String value = keyValue[1];
+
+    if (!"word".equals(key)) {
+      httpExchange.sendResponseHeaders(400, 0);
+    } else {
+      int count = countWord(value);
+
+      byte[] response = Long.toString(count).getBytes();
+      httpExchange.sendResponseHeaders(200, response.length);
+      OutputStream responseBodyOutputStream = httpExchange.getResponseBody();
+      responseBodyOutputStream.write(response);
+
+      // Close the responseBodyOutputStream which will result in sending the response to the client
+      responseBodyOutputStream.close();
+    }
+
+  }
+
+  private int countWord(String value) {
+
+    int count = 0;
+    int index = 0;
+
+    while (index >= 0) {
+      index = textToSearchIn.indexOf(value, index);
+      if (index >= 0) {
+        count++;
+        index++;
+      }
+    }
+
+    return count;
+  }
+}
